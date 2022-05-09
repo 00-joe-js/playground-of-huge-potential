aaa166e46521009ed25fd620b6e269a4a3e5604f
Joe Alves <joseph.michael.alves@gmail.com>
2022-05-09 02:34:22 -0400

    working on FPS controls

--- src/firstPersonCharacter/index.ts
+++ src/firstPersonCharacter/index.ts
@@ -1,14 +1,89 @@
-import { Camera } from "three";
+import { Camera, Quaternion, Vector3, Euler } from "three";
 
 import Keyboard from "./inputHelper";
+
+const canvasElement = document.querySelector("#three-canvas");
+
+const clamp = (min: number, max: number, v: number) => {
+    if (v < min) return min;
+    if (v > max) return max;
+    return v;
+};
+
+if (canvasElement === null) {
+    throw new Error("Document needs #three-canvas.");
+}
+
+let pointerLocked = false;
+canvasElement?.addEventListener("click", () => {
+    canvasElement.requestPointerLock();
+});
+
+canvasElement.addEventListener('pointerlockchange', () => {
+    console.log("it change")
+});
+
+const UP = new Vector3(0, 1, 0);
+const LEFT = new Vector3(-1, 0, 0);
+const RIGHT = new Vector3(1, 0, 0);
+const FORWARD = new Vector3(0, 0, 1);
+const BACKWARD = new Vector3(0, 0, -1);
+
+const _euler = new Euler(0, 0, 0, 'YXZ');
+const _vector = new Vector3(0, 0, 0);
+
+const SPEED = 0.1;
+const MAX_POLAR_ANGLE = Math.PI / 8;
+const MIN_POLAR_ANGLE = -MAX_POLAR_ANGLE;
 
 const setupFPSCharacter = (camera: Camera) => {
 
     const keyboard = new Keyboard();
+    const pointer = { velX: 0.0, velY: 0.0 };
 
-    setInterval(() => {
-        console.log(keyboard);
-    }, 1000);
+    document.addEventListener("mousemove", (e) => {
+        pointer.velX += e.movementX;
+        pointer.velY += e.movementY;
+    });
+
+    return (dt) => {
+
+        if (keyboard.wDown) {
+            console.log(camera.matrix);
+            console.log(_vector);
+            _vector.setFromMatrixColumn(camera.matrix, 0);
+            
+            camera.translateOnAxis(BACKWARD, SPEED);
+        }
+
+        if (keyboard.sDown) {
+            camera.translateOnAxis(FORWARD, SPEED);
+        }
+
+        if (keyboard.aDown) {
+            camera.translateOnAxis(LEFT, SPEED);
+        }
+
+        if (keyboard.dDown) {
+            camera.translateOnAxis(RIGHT, SPEED);
+        }
+
+        if (pointer.velX !== 0 || pointer.velY !== 0) {
+            _euler.setFromQuaternion(camera.quaternion);
+
+            _euler.y -= pointer.velX * 0.002;
+            _euler.x -= pointer.velY * 0.002;
+
+            // _euler.x = Math.max(PI2 - MAX_POLAR_ANGLE, Math.min(PI2 - MIN_POLAR_ANGLE, _euler.x));
+            _euler.x = clamp(MIN_POLAR_ANGLE, MAX_POLAR_ANGLE, _euler.x);
+
+            camera.quaternion.setFromEuler(_euler);
+
+            pointer.velX = 0;
+            pointer.velY = 0;
+        }
+
+    };
 
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import "./style.css";
 
+/* GLOBALS */
+declare global {
+    var PI: number;
+    var PI2: number;
+}
+window.PI = Math.PI;
+window.PI2 = Math.PI * 2;
+
 import { renderLoop } from "./renderer";
-import { Scene, PerspectiveCamera } from "three";
+import { Scene, PerspectiveCamera, MeshLambertMaterial, AmbientLight, DirectionalLight, MeshPhongMaterial } from "three";
 
 import { SphereGeometry, MeshBasicMaterial, Mesh } from "three";
 
@@ -13,7 +21,7 @@ const scene = new Scene();
 const camera = new PerspectiveCamera(50, RESOLUTION, 1, 1000);
 camera.position.z = 10;
 
-setupFPSCharacter(camera);
+const includeInGameLoop = setupFPSCharacter(camera);
 
 let sceneMade = false;
 renderLoop(scene, camera, (dt) => {
@@ -21,9 +29,18 @@ renderLoop(scene, camera, (dt) => {
     if (sceneMade === false) {
         sceneMade = true;
         const sphereG = new SphereGeometry();
-        const material = new MeshBasicMaterial({ color: 0x0000ff });
+        const material = new MeshPhongMaterial({ color: 0x0000ff });
         const sphere = new Mesh(sphereG, material);
         scene.add(sphere);
+
+        scene.add(new AmbientLight(0xffffff, 0.2));
+
+        const directional = new DirectionalLight(0xffff00, 2.5);
+        scene.add(directional);
+
+
     }
+
+    includeInGameLoop(dt);
 
 });
